0af768ea233825d4408ee9063ba1fe7ad9b4a4f6
Mikee Ojastro <mikeeojastro@gmail.com>
2025-03-06 18:53:48 +0800

    March 6, 2025

---
+++ Exercises/Help-WALL-E/app.js
@@ -0,0 +1,10 @@
+const robot = document.querySelector('.robot')
+
+//Challenge: Make Eve move when you click its body.
+
+let positionX = 0;
+function moveRobot() {
+    //add code here
+}
+
+robot.addEventListener('click', moveRobot)
---
+++ Exercises/schoolsout.js
@@ -0,0 +1,9 @@
+function sendText(first, last) {
+    //Write your code here.
+}
+
+sendText('Jose', 'Rizal');
+sendText('Manny', 'Pacquiao');
+sendText('Robert', 'Rodriguez');
+sendText('Elon', 'Musk');
+sendText('Mark', 'Zuckerberg');
